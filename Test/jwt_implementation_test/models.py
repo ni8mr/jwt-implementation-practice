a9ef9dac2dc60bece37c6a6c25240dd5a4d85a22
ni8mr <nilpaddo@gmail.com>
2017-12-06 18:06:59 +0600

    Add a UserDetails table

--- Test/jwt_implementation_test/models.py
+++ Test/jwt_implementation_test/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserDetails(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    phone_no = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(User)
